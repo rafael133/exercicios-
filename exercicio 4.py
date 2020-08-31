070a64e1d1b84c65995e7a6da6ed82c49015c838
Rafael Ferreira <70520991+rafael133@users.noreply.github.com>
2020-08-31 11:42:30 -0300

    Add files via upload

---
+++ exercicio 4.py
@@ -0,0 +1,4 @@
+alunossistemas = int(input('quantidade de alunos que cursam sistemas: '))
+alunosanalise = int(input('quantidade de alunos que cursam analise: '))
+total_alunos = alunossistemas+alunosanalise
+print('total de alunos:',total_alunos)
