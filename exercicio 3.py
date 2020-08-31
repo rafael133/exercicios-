1e03a770a43d5ce404a587b172b9bc2904635fb2
Rafael Ferreira <70520991+rafael133@users.noreply.github.com>
2020-08-31 11:42:14 -0300

    Add files via upload

---
+++ exercicio 3.py
@@ -0,0 +1,5 @@
+produto = str(input('qual o produto: '))
+valor_produto = float(input('valor do produto: '))
+quant_produto = int(input('quantidade do produto: '))
+total = valor_produto*quant_produto
+print('valor total do produto R$:',total,produto,)
